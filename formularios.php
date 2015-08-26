5c2db6cd5eca6dd47b2e3204f5cfcf04325efc80
Duber1234 <dfpesca@misena.edu.co>
2015-08-26 09:51:42 -0500

    eventos

--- formularios.php
+++ formularios.php
@@ -8,55 +8,84 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-       
+
         <script src="js/jquery-2.1.4.min.js"></script>
         <script>
-        
-    
-    
-        
-        function Enviarr(){
-            
-           
-            var n_nombre = document.getElementById("Nombre").value;
-            
-           if(n_nombre ==""){
-               $("#m").text("Porfavor llene este campo");
-           }else{
-               
-               document.write("<?php echo envi() ; ?>");
-               
-           }
-        }
 
-        
+
+
+
+
+            function Enviarr() {
+
+
+                var n_nombre = document.getElementById("Nombre").value;
+                var n_password = document.getElementById("Password").value;
+                var n_username = document.getElementById("Username").value;
+                if (n_nombre == "" || n_password == "" || n_username == "") {
+
+
+
+                    $("#1").text("Porfavor llene este campo").fadeIn(500).hide(1500);
+                    $("#2").text("Porfavor llene este campo").fadeIn(500).hide(1500);
+                    $("#3").text("Porfavor llene este campo").fadeIn(500).hide(1500);
+
+                } else {
+
+
+
+                }
+            }
+
+
         </script>
-        
-       
+
+
     </head>
     <body>
-         
-    <?php
-    
-    function envi(){
-              
-               $_GET['Nombre'] = ' <script>document.getElementById("Nombre").value;</script>  ';
-               echo $_GET['Nombre'];
-               header('Location : ./receptor.php');
-    }
-    
-               
-          
-    ?>
-        
+
+
         <form>
-            
-            <input type="text" id="Nombre" placeholder="Nombre" name="Nombre"/> <label style="color: red" id="m"></label><br>
-            <input type="text" id="Username" placeholder="Username" name="Username" /><label></label><br>
-            <input type="text" id="Password" placeholder="Password" name="Password"/><label></label><br>
+
+            <input  type="text" id="Nombre" placeholder="Nombre" name="Nombre"/> <label  style="color: red" id="1"></label><br>
+            <input type="text" id="Username" placeholder="Username" name="Username" /><label  style="color: red" id="2" ></label><br>
+            <input type="text" id="Password" placeholder="Password" name="Password"/><label  style="color: red" id="3" ></label><br>
             <input type="button" value="Enviar" id="Enviar" onClick="Enviarr()"/>
-            
-            
+
+
         </form>
+        <br>
+        <div id="ban">
+            HOLA !!
+
+            <p  id="cerrar">cerrar</p>
+        </div>
+        <script>
+            var c = document.getElementById("#cerrar");
+            c.addEventListener("click",cerrar_div,false);
+           
+		   
+		    function cerrar_div(){
+               $("#ban").remove();
+
+            }
+
+
+        </script>
+
+
+       <?php
+
+        function envi() {
+
+            $_GET['Nombre'];
+            echo $_GET['Nombre'];
+            header('Location : ./receptor.php');
+        }
+        ?>
+
+
+
+ 
     </body>
 </html>
